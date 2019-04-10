b7f9cce96cc9fff8eeafc1f4c8883ec2a155929b
Brad Beckert <bradbeckert@Mac-That-feat-Eminem.local>
2019-04-10 19:08:37 -0400

    removed python packages from sources, going to switch to requirements.txt that you use in a venv

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bradbeckert/ear-mod"
+set -gx VIRTUAL_ENV "/Users/bradbeckert/Github/ear-mob"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(ear-mod) "
-            printf "%s%s" "(ear-mod) " (set_color normal)
+        if test -n "(ear-mob) "
+            printf "%s%s" "(ear-mob) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
