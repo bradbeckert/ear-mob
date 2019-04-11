ac75ef2a3251f90ba93cc32ff532da41bc6906f4
Brad Beckert <brad@silverthreadinc.com>
2019-04-11 16:59:59 -0400

    things

--- get_sheet.py
+++ get_sheet.py
@@ -1,9 +1,15 @@
 import gsheets
 from gsheets import Sheets
+import pandas as pd
 
 gsheets.get_credentials()
 sheets = Sheets.from_files('~/client_secrets.json', '~/storage.json')
 sheets  #doctest: +ELLIPSIS
+
 s = sheets.get('https://docs.google.com/spreadsheets/d/1Ag4RYqsYs3iqWsXqaDiWklDToUIXorakxkP5jb85bEw')
 s.find('Album List')
 s.sheets[0].to_csv('Ratings.csv', encoding='utf-8', dialect='excel')
+
+df = s.sheets[0].to_frame()
+pd.set_option('display.max_columns', 10)
+print(df)
